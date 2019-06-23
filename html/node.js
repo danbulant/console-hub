8181ee3c1f37416f21d94585479f13a9e3df577b
danbulant <dan@bulant.tk>
2019-06-23 10:20:20 +0200

    fix "require is undefined" in browsers without node

--- html/node.js
+++ html/node.js
@@ -1,23 +1,26 @@
+if(typeof require() != undefined){
+  var wifi, wifiQuality;
+  // In renderer process (web page).
+  const { ipcRenderer } = require('electron')
 
-var wifi, wifiQuality;
-// In renderer process (web page).
-const { ipcRenderer } = require('electron')
-
-ipcRenderer.on('wifi', (event, arg) => {
-  wifi = arg;
-  wifi = wifi;
-})
-ipcRenderer.on('wifiQuality', (event, arg) => {
-  wifiQuality = arg;
-  wifiQuality = wifiQuality;
-})
-ipcRenderer.on('fullscreen', (event, arg) => {
-  console.log("Fullscreen now " + arg);
-  settingMenuItemValues[0] = arg;
-})
-function sendFullscreen(bool){
-  console.log("Sending fullscreen " + bool);
-  ipcRenderer.send('fullscreen', bool);
+  ipcRenderer.on('wifi', (event, arg) => {
+    wifi = arg;
+    wifi = wifi;
+  })
+  ipcRenderer.on('wifiQuality', (event, arg) => {
+    wifiQuality = arg;
+    wifiQuality = wifiQuality;
+  })
+  ipcRenderer.on('fullscreen', (event, arg) => {
+    console.log("Fullscreen now " + arg);
+    settingMenuItemValues[0] = arg;
+  })
+  function sendFullscreen(bool){
+    console.log("Sending fullscreen " + bool);
+    ipcRenderer.send('fullscreen', bool);
+  }
+  ipcRenderer.send('get-data', 'wifi');
+  ipcRenderer.send('get-data', 'wifiQuality');
+} else {
+  //no NODE integration, propably browser access
 }
-ipcRenderer.send('get-data', 'wifi');
-ipcRenderer.send('get-data', 'wifiQuality');
